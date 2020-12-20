470363d7f8643eb29b1c2ab3b549bf07150fd008
zpr1g <yuns.xie@qq.com>
2020-12-20 12:16:13 +0800

    refactor: extract data_source

--- src/plugins/tuling/__init__.py
+++ src/plugins/tuling/__init__.py
@@ -1,54 +1,14 @@
-import json
-import aiohttp
 import nonebot
-import hashlib
 import random
 from aiocqhttp.message import escape
-from nonebot import on_command, on_message
+from nonebot import on_message
 from nonebot.rule import to_me
 from nonebot.adapters.cqhttp import Bot, Event
-from nonebot.log import logger
-from typing import Sequence, Optional, Any
+from typing import Sequence
 
-from .config import Config
-
-global_config = nonebot.get_driver().config
-plugin_config = Config(**global_config.dict())
+from .data_source import call_tuling_api
 
 tuling = on_message(rule=to_me(), priority=999)
-
-
-def context_id(event: Event, *, mode: str = 'default',
-               use_hash: bool = False) -> str:
-    """
-    Calculate a unique id representing the context of the given event.
-    mode:
-      default: one id for one context
-      group: one id for one group or discuss
-      user: one id for one user
-    :param event: the event object
-    :param mode: unique id mode: "default", "group", or "user"
-    :param use_hash: use md5 to hash the id or not
-    """
-    ctx_id = ''
-    if mode == 'default':
-        if event.group_id:
-            ctx_id = f'/group/{event.group_id}'
-        if event.user_id:
-            ctx_id += f'/user/{event.user_id}'
-    elif mode == 'group':
-        if event.group_id:
-            ctx_id = f'/group/{event.group_id}'
-        elif event.user_id:
-            ctx_id = f'/user/{event.user_id}'
-    elif mode == 'user':
-        if event.user_id:
-            ctx_id = f'/user/{event.user_id}'
-
-    if ctx_id and use_hash:
-        ctx_id = hashlib.md5(ctx_id.encode('ascii')).hexdigest()
-    return ctx_id
-
 
 def render_expression(expr,
                       *args,
@@ -99,46 +59,3 @@ async def handle_tuling(bot: Bot, event: Event, state: dict):
         # 如果调用失败，或者它返回的内容我们目前处理不了，发送无法获取图灵回复时的「表达」
         # 这里的 render_expression() 函数会将一个「表达」渲染成一个字符串消息
         await tuling.reject(render_expression(EXPR_DONT_UNDERSTAND))
-
-
-async def call_tuling_api(event: Event, text: str) -> Optional[str]:
-    if not text:
-        return None
-
-    url = 'http://openapi.tuling123.com/openapi/api/v2'
-
-    # 构造请求数据
-    payload = {
-        'reqType': 0,
-        'perception': {
-            'inputText': {
-                'text': text
-            }
-        },
-        'userInfo': {
-            'apiKey': plugin_config.API_KEY,
-            'userId': context_id(event, use_hash=True)
-        }
-    }
-
-    group_unique_id = context_id(event, mode='group', use_hash=True)
-    if group_unique_id:
-        payload['userInfo']['groupId'] = group_unique_id
-
-    try:
-        # 使用 aiohttp 库发送最终的请求
-        async with aiohttp.ClientSession() as sess:
-            async with sess.post(url, json=payload) as response:
-                if response.status != 200:
-                    # 如果 HTTP 响应状态码不是 200，说明调用失败
-                    return None
-
-                resp_payload = json.loads(await response.text())
-                if resp_payload['results']:
-                    for result in resp_payload['results']:
-                        if result['resultType'] == 'text':
-                            # 返回文本类型的回复
-                            return result['values']['text']
-    except (aiohttp.ClientError, json.JSONDecodeError, KeyError):
-        # 抛出上面任何异常，说明调用失败
-        return None
---
+++ src/plugins/tuling/data_source.py
@@ -0,0 +1,85 @@
+import json
+import aiohttp
+import nonebot
+import hashlib
+from nonebot.adapters.cqhttp import Event
+from nonebot.log import logger
+from typing import Optional
+
+from .config import Config
+
+global_config = nonebot.get_driver().config
+plugin_config = Config(**global_config.dict())
+
+def context_id(event: Event, *, mode: str = 'default',
+               use_hash: bool = False) -> str:
+    """
+    Calculate a unique id representing the context of the given event.
+    mode:
+      default: one id for one context
+      group: one id for one group or discuss
+      user: one id for one user
+    :param event: the event object
+    :param mode: unique id mode: "default", "group", or "user"
+    :param use_hash: use md5 to hash the id or not
+    """
+    ctx_id = ''
+    if mode == 'default':
+        if event.group_id:
+            ctx_id = f'/group/{event.group_id}'
+        if event.user_id:
+            ctx_id += f'/user/{event.user_id}'
+    elif mode == 'group':
+        if event.group_id:
+            ctx_id = f'/group/{event.group_id}'
+        elif event.user_id:
+            ctx_id = f'/user/{event.user_id}'
+    elif mode == 'user':
+        if event.user_id:
+            ctx_id = f'/user/{event.user_id}'
+
+    if ctx_id and use_hash:
+        ctx_id = hashlib.md5(ctx_id.encode('ascii')).hexdigest()
+    return ctx_id
+
+async def call_tuling_api(event: Event, text: str) -> Optional[str]:
+    logger.info(f'input text: {text}')
+    if not text:
+        return None
+
+    # 构造请求数据
+    payload = {
+        'reqType': 0,
+        'perception': {
+            'inputText': {
+                'text': text
+            }
+        },
+        'userInfo': {
+            'apiKey': plugin_config.API_KEY,
+            'userId': context_id(event, use_hash=True)
+        }
+    }
+
+    group_unique_id = context_id(event, mode='group', use_hash=True)
+    if group_unique_id:
+        payload['userInfo']['groupId'] = group_unique_id
+
+    try:
+        # 使用 aiohttp 库发送最终的请求
+        async with aiohttp.ClientSession() as sess:
+            async with sess.post(plugin_config.API_URL, json=payload) as response:
+                if response.status != 200:
+                    # 如果 HTTP 响应状态码不是 200，说明调用失败
+                    return None
+
+                resp_payload = json.loads(await response.text())
+                logger.info(f'response: {resp_payload}')
+                if resp_payload['results']:
+                    for result in resp_payload['results']:
+                        if result['resultType'] == 'text':
+                            # 返回文本类型的回复
+                            return result['values']['text']
+    except (aiohttp.ClientError, json.JSONDecodeError, KeyError):
+        # 抛出上面任何异常，说明调用失败
+        return None
