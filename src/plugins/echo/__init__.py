d4422b209ebf9cff7dd8ce9a12291aaae4df6cea
Yuns <yuns.xie@qq.com>
2020-12-23 20:27:09 +0800

    feat: init echo plugin

---
+++ src/plugins/echo/__init__.py
@@ -0,0 +1,46 @@
+import re
+from nonebot import on_message
+from nonebot.rule import to_me
+from nonebot.adapters.cqhttp import Bot, Event, escape
+from typing import Sequence
+
+echo = on_message(priority=20)
+
+echo_maps = [
+    {
+        'reply': '确实',
+        'rules': [
+            '不会',
+            '应该',
+            'YYDS',
+            re.compile('.[^?？][?？]$')
+        ]
+    },
+    {
+        'reply': '羡慕',
+        'rules': [
+            'XM',
+            '羡慕'
+        ]
+    },
+    {
+        'reply': '我信了',
+        'rules': [
+            'flag',
+        ]
+    }
+]
+
+
+@echo.handle()
+async def handle_indeed(bot: Bot, event: Event, state: dict):
+    message = str(event.message)
+
+    for echo_map in echo_maps:
+        for rule in echo_map['rules']:
+            if type(rule) is re.Pattern and rule.search(message):
+                await echo.finish(echo_map['reply'])
+                return
+            elif type(rule) is str and rule in message:
+                await echo.finish(echo_map['reply'])
+                return
