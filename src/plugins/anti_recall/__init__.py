b287a772e9f01b8f9696581fecf5346439c30111
Yuns <yuns.xie@qq.com>
2021-01-10 20:25:57 +0800

    feat: nickname_in_group

--- src/plugins/anti_recall/__init__.py
+++ src/plugins/anti_recall/__init__.py
@@ -3,6 +3,7 @@ from nonebot.rule import Rule
 from nonebot.adapters.cqhttp import Bot, Event, GroupRecallNoticeEvent, FriendRecallNoticeEvent, Message
 from nonebot.typing import T_State, Union
 from nonebot.log import logger
+from typing import Dict, Any
 
 
 async def recall_checker(bot: Bot, event: Event, state: T_State) -> bool:
@@ -16,13 +17,8 @@ anti_recall = on_notice(rule=Rule(recall_checker), priority=10)
 async def handle_anti_recall(bot: Bot, event: Union[GroupRecallNoticeEvent, FriendRecallNoticeEvent], state: T_State):
     message_detail = await bot.get_msg(message_id=event.message_id)
 
-    # user_id = message_detail['sender']['user_id']
-    nickname = message_detail['sender']['nickname']
-    group_id = message_detail['group_id']
-    group_name = (await bot.get_group_info(group_id=group_id))['group_name'] if group_id else None
-    at_group = f'#{group_name}' if group_name else None
-
-    user_text = ' '.join(filter(lambda x: x, [f'@{nickname}', f'{at_group}', '撤回了：\n']))
+    user_info = await get_user_info(message_detail, bot)
+    user_text = ' '.join(filter(lambda x: x, user_info + ['撤回了：\n']))
 
     user_segment = {"type": "text", 'data': {'text': user_text}}
     message_segment = message_detail['message']
@@ -31,3 +27,14 @@ async def handle_anti_recall(bot: Bot, event: Union[GroupRecallNoticeEvent, Frie
 
     for superuser in bot.config.superusers:
         await bot.send_private_msg(user_id=int(superuser), message=Message([user_segment] + message_segment))
+
+
+async def get_user_info(message: Dict[str, Any], bot: Bot):
+    user_id = message['sender']['user_id']
+    nickname = message['sender']['nickname']
+    group_id = message['group_id']
+    if group_id:
+        group_name = (await bot.get_group_info(group_id=group_id))['group_name']
+        nickname_in_group = (await bot.get_group_member_info(group_id=group_id, user_id=user_id))
+        return [nickname_in_group, group_name]
+    return [nickname]
